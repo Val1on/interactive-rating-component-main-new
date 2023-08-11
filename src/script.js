98c6b67cad42c9159f9357901ec21f1ae6e0cc56
Cyrus Ballon <118657085+Val1on@users.noreply.github.com>
2023-08-12 00:05:05 +0800

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,21 +1,26 @@
 const rating = document.querySelector(".main__numbers li");
+const button = document.querySelector(".main__btn");
+const thankSection = document.querySelector(".main__thanks");
+const ratingSection = document.querySelector(".main__survey");
+const ratedNumber = document.getElementById(".main__rated");
+let selected = null;
 
-// rating.forEach(ratingNumbers => {
-//     ratingNumbers
-// });
-
-function ratingNumbers() {
-  rating.forEach();
-  ratingNumbers.addEventListener("click", e());
-
-  function e() {
-    const active = document.querySelector(".checked");
-    if (active) {
-      active.classList.remove("checked");
+rating.forEach((ratingNumbers) => {
+  ratingNumbers.addEventListener("click", (e) => {
+    const colorFocus = document.querySelector(".main__focus");
+    if (colorFocus) {
+      rating.classList.remove("main__focus");
     }
-    const card = e.target;
-    card.classList.add("checked");
-    rate = e.target.innerText;
-    console.log(rate);
+    const select = e.target;
+    select.classList.add("main__focus");
+    selected = e.target.innerText;
+  });
+});
+
+button.addEventListener("submit", () => {
+  if (selected) {
+    selected = ratedNumber.innerText;
+    ratingSection.classList.add("main__hidden");
+    thankSection.classList.remove("main__hidden");
   }
-}
+});
